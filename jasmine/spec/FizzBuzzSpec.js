353e26426cd56b414cdd275c02876ce2411f1dbe
Unai Motriko <unaimotriko@gmail.com>
2017-05-09 13:58:37 +0100

    It provides the FizzBuzz game mechanics

--- jasmine/spec/FizzBuzzSpec.js
+++ jasmine/spec/FizzBuzzSpec.js
@@ -1,14 +1,46 @@
 describe('FizzBuzz', function() {
 
 var fizzbuzz;
+beforeEach(function(){
+    fizzbuzz = new FizzBuzz();
+});
 
 describe('knows when a number is', function() {
 
   it('divisible by 3', function() {
-    fizzbuzz = new FizzBuzz();
-    expect(isDivisibleByThree(3)).toBe(true);
+    expect(fizzbuzz.isDivisibleByThree(3)).toBe(true);
     });
 
   });
+
+describe ('knows when a number is divisible by 5', function(){
+
+  it('is divisible by 5', function() {
+    expect(fizzbuzz.isDivisibleByFive(5)).toBe(true);
+  });
+
+});
+describe ('knows when a number is divisible by 15', function (){
+  it('is divisible by 15', function(){
+    expect(fizzbuzz.isDivisibleByFifteen(15)).toBe(true);
+    });
+  });
+
+describe('when playing it calls Fizz, Buzz or Fizzbuzz', function(){
+  it('3 returns Fizz', function(){
+    expect(fizzbuzz.says(3)).toEqual("Fizz");
+  });
+  it('5 returns Buzz', function(){
+    expect(fizzbuzz.says(5)).toEqual("Buzz");
+  });
+  it('15 returns FizzBuzz', function(){
+    expect(fizzbuzz.says(15)).toEqual("FizzBuzz")
+  });
+});
+
+
+
+
+
 
 });
--- jasmine/src/FizzBuzz.js
+++ jasmine/src/FizzBuzz.js
@@ -1,1 +1,32 @@
-function FizzBuzz(){};
+function FizzBuzz () {
+
+};
+
+FizzBuzz.prototype._isDivisibleBy = function (number, divisor ){
+  return ( number % divisor === 0);
+};
+
+FizzBuzz.prototype.isDivisibleByThree = function (number) {
+  return this._isDivisibleBy(number, 3);
+};
+
+FizzBuzz.prototype.isDivisibleByFive = function (number) {
+  return this._isDivisibleBy(number, 5);
+};
+
+FizzBuzz.prototype.isDivisibleByFifteen = function (number) {
+  return this._isDivisibleBy(number, 15);
+};
+
+FizzBuzz.prototype.says = function (number){
+  if(this.isDivisibleByFifteen(number)){
+  return "FizzBuzz";
+}
+  if (this.isDivisibleByThree(number)){
+    return "Fizz";
+  }
+  if(this.isDivisibleByFive(number)){
+    return "Buzz";
+  }
+  return number;
+};
